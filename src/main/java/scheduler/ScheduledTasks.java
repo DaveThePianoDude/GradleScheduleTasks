b420811b02f1e01332618f3fbd1b2382552d5853
David Holland <dah_7@yahoo.com>
2016-04-28 12:07:26 -0400

    Task now prints json response.

--- src/main/java/scheduler/ScheduledTasks.java
+++ src/main/java/scheduler/ScheduledTasks.java
@@ -1,10 +1,19 @@
 package scheduler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 @Component
 public class ScheduledTasks {
@@ -16,6 +25,25 @@ public class ScheduledTasks {
   
     System.out.println("The time is now " + dateFormat.format(new Date()));
 
+	try {
+
+      DefaultHttpClient httpClient = new DefaultHttpClient();
+      HttpGet getRequest = new HttpGet("http://192.168.1.9:9200/");
+      HttpResponse response = httpClient.execute(getRequest);
+	  String responseAsString = EntityUtils.toString(response.getEntity());
+      System.out.println("Response: " + responseAsString);
+      httpClient.getConnectionManager().shutdown();
+   
+    } catch (ClientProtocolException e) {
+    
+      e.printStackTrace();
+    
+    } catch (IOException e) {
+
+      e.printStackTrace();     
+
+    }
+	
   }
 
 }
